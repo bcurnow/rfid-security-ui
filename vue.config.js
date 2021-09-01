3f9de1970a9a4f41fd601deefc7ab8daa848d21a
“Brian <bcurnow@users.noreply.github.com>
2021-09-01 17:40:22 +0100

    Made public configurable to enable docker-compose, turned on historyApiFallback since we're actually using that, enabled during off the host check (again for docker-compose), turned off auto-reload as I haven't figured out how to make it work through nginx yet

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
   devServer: {
     host: '0.0.0.0',
     hot: true,
-    public: 'devpi.local',
+    public: process.env.WEBPACK_PUBLIC || 'devpi.local',
+    historyApiFallback: true,
+    disableHostCheck: process.env.WEBPACK_DISABLE_HOST_CHECK == 'true' || false,
+    reload: false,
   },
 }
