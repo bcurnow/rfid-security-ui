7170bec549e7e1668921470816ae9a15df555e58
“Brian <bcurnow@users.noreply.github.com>
2021-09-05 04:25:25 +0100

    Updated to be a very simple welcome page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,5 @@
 <template>
   <div>
     <p>Welcome to RFID Security Manager!</p>
-    <ul>
-      <li v-for="item in thelist" :key="item.message">
-        {{item}}
-      </li>
-    </ul>
-
   </div>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      thelist: [
-        {message: "hi!"}
-      ]
-    }
-  },
-  mounted() {
-    const url="http://devpi.local:8080/api/v1.0/media/049B558AB35F80"
-    this.axios.get(url, {
-      header: {
-        'X-RFIDSECURITYSVC-API-KEY': this.$config.apiKey,
-      }
-    })
-    .then(response => {
-      this.thelist = [
-        response.data
-      ]
-    })
-    .catch(function(error) {
-      console.log(error)
-    })
-  }
-}
-</script>
