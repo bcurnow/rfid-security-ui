37a7f1ed3ddd6bc24e8f55d0545fdd7f6c8e8507
“Brian <bcurnow@users.noreply.github.com>
2021-09-05 04:25:05 +0100

    Added media and config routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/Home.vue'
+import MediaList from '@/views/media/MediaList.vue'
+import MediaEdit from '@/views/media/MediaEdit.vue'
+import ConfigList from '@/views/config/ConfigList.vue'
+import ConfigEdit from '@/views/config/ConfigEdit.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +12,17 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+  },
+  {
+    path: '/media',
+    name: 'MediaList',
+    component: MediaList,
+  },
+  {
+    path: '/config',
+    name: 'ConfigList',
+    component: ConfigList,
   },
 ]
 
