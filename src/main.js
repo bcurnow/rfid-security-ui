8989689cab95b2c0853fd562381f0c3321fe7682
“Brian <bcurnow@users.noreply.github.com>
2021-09-05 04:23:36 +0100

    Added global for the service API, turned off devtools warnings

--- src/main.js
+++ src/main.js
@@ -6,13 +6,20 @@ import App from './App.vue'
 import router from './router'
 import config from './config'
 import {name, version} from '../package'
+import RFIDSecuritySvc from './components/RFIDSecuritySvc.js'
+
 
 config.appVersion = name + ' v' + version
 
 Vue.config.productionTip = config.production || false
+// Turn off the annoying Vue extension message
+Vue.config.devtools = false
 
 // Add the global vars for the application
 Vue.prototype.$config = config
+
+// Add the global vars for the services
+Vue.prototype.$RFIDSecuritySvc = RFIDSecuritySvc
 
 new Vue({
   router,
