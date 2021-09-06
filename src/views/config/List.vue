12ef727f28a8ea02a7a8ce8d5bd79bbceb1dad55
“Brian <bcurnow@users.noreply.github.com>
2021-09-06 02:21:28 +0100

    Updated to use the common list and details views

--- src/views/config/List.vue
+++ src/views/config/List.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="container text-left">
-    <item-list :deleteItem="deleteItem" itemType="Configuration" :fields="fields" :itemsPromise="itemsPromise" :newItem="newItem" primary-key="key">
-      <template v-slot:detailsModal="slotProps">
-        <item-details :itemAddedCallback="slotProps.itemAdded"></item-details>
+    <item-list :deleteItemPromise="deleteItemPromise" itemType="Configuration" :fields="fields" :itemsPromise="itemsPromise" :newItem="newItem" primaryKey="key" :controlFilter="controlFilter" :errorResolver="errorResolver">
+      <template v-slot:detailsModal="detailsModalProps">
+        <item-details :itemAddedCallback="detailsModalProps.itemAdded" itemType="Configuration" :createItemPromise="createItemPromise" :updateItemPromise="updateItemPromise" :validationStates="validationStates" :errorResolver="errorResolver">
+          <template v-slot:itemFormGroups="formGroupProps">
+            <b-form-group label="Key:" label-for="key-input" invalid-feedback="A configuration key is required!">
+              <b-form-input id="key-input" v-model="formGroupProps.item.key" v-if="!formGroupProps.newItem" plaintext></b-form-input>
+              <b-form-input id="key-input" v-model="formGroupProps.item.key" v-if="formGroupProps.newItem" :state="validationStates.keyState" placeholder="Config Key" required></b-form-input>
+            </b-form-group>
+            <b-form-group label="Value:" label-for="value-input" invalid-feedback="A configuration value is required!">
+              <b-form-input id="value-input" v-model="formGroupProps.item.value" :state="validationStates.valueState" placeholder="Config Value" required></b-form-input>
+            </b-form-group>
+          </template>
+        </item-details>
       </template>
     </item-list>
   </div>
 </template>
 <script>
   import List from '../common/List'
-  import Details from './Details'
+  import Details from '../common/Details'
 
   export default {
     components: {
@@ -18,6 +28,10 @@
     },
     data() {
       return {
+        controlFilter: [
+          'ADMIN_API_KEY',
+        ],
+        errorResolver: this.$RFIDSecuritySvc.errorToString,
         fields: [
           {
             key: 'key',
@@ -33,10 +47,17 @@
             label: '',
           }
         ],
+        validationStates: {
+          keyState: null,
+          valueState: null,
+        }
       }
     },
     methods: {
-      deleteItem: function(item) {
+      createItemPromise: function(item) {
+        return this.$RFIDSecuritySvc.config.create(item)
+      },
+      deleteItemPromise: function(item) {
         return this.$RFIDSecuritySvc.config.delete(item.key)
       },
       itemsPromise: function() {
@@ -45,6 +66,9 @@
       newItem: () => {
         return { key: null }
       },
+      updateItemPromise: function(item) {
+        return this.$RFIDSecuritySvc.config.update(item.key, item.value)
+      }
     },
   }
 </script>
