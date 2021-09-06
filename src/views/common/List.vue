889751afb47fef56bd912908370744014d16a79c
“Brian <bcurnow@users.noreply.github.com>
2021-09-06 02:20:26 +0100

    Updated name of the detailItem function to be consistent with the other promise functions, added error resolver and better error handling, added controlFilter to allow the controls to be filtered out for some keys.

--- src/views/common/List.vue
+++ src/views/common/List.vue
@@ -5,7 +5,7 @@
       <div class="col-sm">
         <b-table id="CommonList" responsive="md" :items="items" :fields="fields" :primary-key="primaryKey" :sort-by="primaryKey" head-row-variant="primary" sort-icon-left hover>
           <template #cell(controls)="data">
-            <slot name="controlsCell">
+            <slot name="controlsCell" v-if="includeControls(data.item[primaryKey])">
               <b-button size="sm" v-b-modal.item-details variant="primary" @click="editItem(data.item)" pill><b-icon icon="pencil"></b-icon></b-button>
               <b-button v-if="hasDeleteFunction" size="sm" class="ml-1" variant="danger" @click="handleDelete(data.item)" pill><b-icon icon="dash-circle-fill"></b-icon></b-button>
             </slot>
@@ -22,7 +22,7 @@
   export default {
     computed: {
       hasDeleteFunction: function() {
-        return this.deleteItem && this.itemType
+        return this.deleteItemPromise && this.itemType
       }
     },
     data() {
@@ -43,12 +43,12 @@
         })
         .then(value => {
           if (value) {
-            this.deleteItem(item)
+            this.deleteItemPromise(item)
             .then(() => {
               this.items.splice(this.items.findIndex(otherItem => otherItem[this.primaryKey] === item[this.primaryKey]), 1)
             })
             .catch(err => {
-              this.$bvModal.msgBoxOk(`Unable to delete ${this.itemType.toLowerCase()} '${item[this.primaryKey]}': ${err}`)
+              this.$bvModal.msgBoxOk(`Unable to delete ${this.itemType.toLowerCase()} '${item[this.primaryKey]}': ${this.errorResolver(err)}`)
             })
           }
         })
@@ -56,12 +56,27 @@
           console.log(err)
         })
       },
+      includeControls(key) {
+        if (this.controlFilter.findIndex(filterKey => filterKey === key) > -1) {
+          return false
+        }
+        return true
+      },
       itemAdded(item) {
         this.items.push(item)
       },
     },
     props: {
-      deleteItem: Function,
+      deleteItemPromise: Function,
+      errorResolver: {
+        type: Function,
+        default: function(error) {
+          if (error) {
+            return error.toString()
+          }
+          return ""
+        }
+      },
       itemType: String,
       fields: {
         type: Array,
@@ -81,6 +96,12 @@
         type: String,
         required: true,
       },
+      controlFilter: {
+        type: Array,
+        default: function() {
+          return []
+        },
+      }
     },
     mounted() {
       this.itemsPromise()
