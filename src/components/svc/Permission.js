32d5cdf072c0861261756be6a89cef91c1ea47a5
“Brian <bcurnow@users.noreply.github.com>
2021-09-06 04:33:27 +0100

    initial add

---
+++ src/components/svc/Permission.js
@@ -0,0 +1,27 @@
+import api from './Base.js'
+import combineURLs from 'axios/lib/helpers/combineURLs.js'
+
+const BASE_URL = '/permission'
+
+const svc = {
+  api: api,
+  create: function(data) {
+    return api.post(BASE_URL, data)
+  },
+  delete: function(pk) {
+    return api.delete(combineURLs(BASE_URL, String(pk)), {})
+  },
+  get: function(pk) {
+    return api.get(combineURLs(BASE_URL, String(pk)), {})
+  },
+  list: function() {
+    return api.get(BASE_URL, {})
+  },
+  update: function(pk, name, desc) {
+    return api.put(combineURLs(BASE_URL, String(name)), {
+      desc: desc,
+    })
+  }
+}
+
+export default svc
