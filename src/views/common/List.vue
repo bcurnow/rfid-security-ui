c2e8bb3a07ffad78ed8745a8662895c3c716c8e3
“Brian <bcurnow@users.noreply.github.com>
2021-09-06 01:04:55 +0100

    Removed debug logging, cleaned up props and removed references to config

--- src/views/common/List.vue
+++ src/views/common/List.vue
@@ -3,23 +3,28 @@
     <b-button size="md" v-b-modal.item-details variant="primary" @click="createItem" pill><b-icon icon="plus-circle-fill"></b-icon> Add</b-button>
     <div class="row">
       <div class="col-sm">
-        <b-table id="CommonList" responsive="md" :items="items" :fields="fields" primary-key="primaryKey" sort-by="primaryKey" head-row-variant="primary" sort-icon-left hover>
+        <b-table id="CommonList" responsive="md" :items="items" :fields="fields" :primary-key="primaryKey" :sort-by="primaryKey" head-row-variant="primary" sort-icon-left hover>
           <template #cell(controls)="data">
             <slot name="controlsCell">
               <b-button size="sm" v-b-modal.item-details variant="primary" @click="editItem(data.item)" pill><b-icon icon="pencil"></b-icon></b-button>
-              <b-button size="sm" class="ml-1" variant="danger" @click="deleteItem(data.item)" pill><b-icon icon="dash-circle-fill"></b-icon></b-button>
+              <b-button v-if="hasDeleteFunction" size="sm" class="ml-1" variant="danger" @click="handleDelete(data.item)" pill><b-icon icon="dash-circle-fill"></b-icon></b-button>
             </slot>
           </template>
         </b-table>
       </div>
     </div>
-    <slot name="detailsModal" v-on:item-added="itemAdded" v-on:item-deleted="itemDeleted">
+    <slot name="detailsModal" v-bind:itemAdded="itemAdded">
       <h1>No modal content supplied!</h1>
     </slot>
   </div>
 </template>
 <script>
   export default {
+    computed: {
+      hasDeleteFunction: function() {
+        return this.deleteItem && this.itemType
+      }
+    },
     data() {
       return {
         items: [],
@@ -27,31 +32,58 @@
     },
     methods: {
       createItem() {
-        console.log('createItem')
-        this.$emit('item-selected', this.newItem())
+        this.$store.commit('updateListDetailShared', {item: this.newItem(), newItem: true})
       },
       editItem(item) {
-        console.log(`editItem: ${item}`)
-        this.$emit('item-selected', item)
+        this.$store.commit('updateListDetailShared', {item: item, newItem: false})
+      },
+      handleDelete(item) {
+        this.$bvModal.msgBoxConfirm(`Are you sure you want to delete '${item[this.primaryKey]}'?`, {
+          title: `Delete ${this.itemType}?`,
+        })
+        .then(value => {
+          if (value) {
+            this.deleteItem(item)
+            .then(() => {
+              this.items.splice(this.items.findIndex(otherItem => otherItem[this.primaryKey] === item[this.primaryKey]), 1)
+            })
+            .catch(err => {
+              this.$bvModal.msgBoxOk(`Unable to delete ${this.itemType.toLowerCase()} '${item[this.primaryKey]}': ${err}`)
+            })
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
       },
       itemAdded(item) {
-        console.log(`itemAdded: ${item}`)
         this.items.push(item)
       },
-      itemDeleted(item) {
-        console.log(`itemDeleted: ${item}`)
-        this.items.splice(this.items.findIndex(otherItem => otherItem[this.primaryKey] === item[this.primaryKey]), 1)
-      }
     },
     props: {
       deleteItem: Function,
-      fields: Array,
-      itemPromise: Function,
-      newItem: Function,
-      primaryKey: String,
+      itemType: String,
+      fields: {
+        type: Array,
+        required: true
+      },
+      itemsPromise: {
+        type: Function,
+        required: true,
+      },
+      newItem: {
+        type: Function,
+        default: function() {
+          return {}
+        }
+      },
+      primaryKey: {
+        type: String,
+        required: true,
+      },
     },
     mounted() {
-      this.itemPromise()
+      this.itemsPromise()
       .then(response => {
         this.items = response.data
       })
