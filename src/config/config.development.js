072fc769c9819281cb79b680a185f0936867fadd
“Brian <bcurnow@users.noreply.github.com>
2021-09-14 21:25:30 +0100

    Updated the default value to use the proxy value

--- src/config/config.development.js
+++ src/config/config.development.js
@@ -1,7 +1,7 @@
 const config = {
   apiKey: process.env.VUE_APP_RFID_API_KEY || 'Wu0sWRGCLF1NClIsYZVCdc-MV8b696oivfFF1oQqCGnK0vEaUZyb80El0ln6eCWKO9qrNlNTmvOfb7q0-GtnTw',
   production: false,
-  apiUrl: process.env.VUE_APP_RFID_API_URL || 'http://rfidsecuritysvc:5000/api/v1.0/'
+  apiUrl: process.env.VUE_APP_RFID_API_URL || 'http://devpi.local:8080/api/v1.0/'
 }
 
 export default config
