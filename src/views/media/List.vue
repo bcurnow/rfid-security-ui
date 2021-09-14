412d52dfd1c07663f1756fdadfcec276bfc51ea4
“Brian <bcurnow@users.noreply.github.com>
2021-09-14 21:26:46 +0100

    Added button to trigger a read

--- src/views/media/List.vue
+++ src/views/media/List.vue
@@ -23,14 +23,21 @@
         >
           <template v-slot:itemFormGroups>
             <b-form-group label="ID:" label-for="id-input" invalid-feedback="A media ID is required!">
-              <b-form-input id="id-input" v-model="detailsModalProps.selected.id" v-if="!detailsModalProps.isNew" plaintext></b-form-input>
-              <b-form-input id="id-input" v-model="detailsModalProps.selected.id" v-if="detailsModalProps.isNew" :state="validationStates.idState" placeholder="Media ID" required></b-form-input>
+              <b-input-group>
+                <b-form-input id="id-input" v-model="detailsModalProps.selected.id" v-if="!detailsModalProps.isNew" plaintext></b-form-input>
+                <b-form-input id="id-input" v-model="detailsModalProps.selected.id" v-if="detailsModalProps.isNew" :state="validationStates.idState" placeholder="Media ID" ref="mediaId" required></b-form-input>
+                <b-input-group-append>
+                  <b-button variant="info" @click="readMedia(detailsModalProps.selected)" v-if="detailsModalProps.isNew">Read Media</b-button>
+                </b-input-group-append>
+              </b-input-group>
+              <b-toast id="readingToast" variant="info" toaster="readingToaster" static solid no-close-button no-auto-hide>Please place the media near the reader</b-toast>
+              <b-toast id="readingErrorToast" variant="danger" toaster="readingToaster" static solid no-close-button no-auto-hide>{{ mediaReadError }}</b-toast>
             </b-form-group>
             <b-form-group label="Name:" label-for="name-input" invalid-feedback="A media name is required!">
               <b-form-input id="name-input" v-model="detailsModalProps.selected.name" :state="validationStates.nameState" placeholder="Media Name" required></b-form-input>
             </b-form-group>
             <b-form-group label="Description:" label-for="desc-input">
-              <b-form-input id="desc-input" v-model="detailsModalProps.selected.desc" placeholder="Media Description"></b-form-input>
+              <b-form-textarea id="desc-input" v-model="detailsModalProps.selected.desc" placeholder="Media Description"></b-form-textarea >
             </b-form-group>
           </template>
         </item-details>
@@ -69,6 +76,7 @@
           }
         ],
         itemType: "Media",
+        mediaReadError: "",
         primaryKey: "id",
         validationStates: {
           idState: null,
@@ -89,9 +97,28 @@
       itemsPromise: function() {
         return this.$RFIDSecuritySvc.media.list()
       },
+      readMedia: function() {
+        this.$bvToast.hide("readingErrorToast")
+        this.$bvToast.show('readingToast')
+
+        this.$RFIDSecuritySvc.reader.getUid()
+        .then(response => {
+          // I'm not sure this is the best way to do this but it does work
+          let el = document.getElementById('id-input')
+          el.value=response.data
+          el.dispatchEvent(new Event('input'));
+          this.$bvToast.hide("readingToast")
+        })
+        .catch(err => {
+          this.mediaReadError = `An error occurred while reading media: ${err}`
+          this.$bvToast.hide("readingToast")
+          this.$bvToast.show('readingErrorToast')
+          console.log(err)
+        })
+      },
       updateItemPromise: function(item) {
         return this.$RFIDSecuritySvc.media.update(item.id, item.name, item.desc)
-      }
+      },
     },
   }
 </script>
