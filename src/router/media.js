aae7ea458ee0a1d3d131debdc30b598bc09decdc
“Brian <bcurnow@users.noreply.github.com>
2021-09-17 22:34:16 +0100

    Added route for editing permissions associated with media

--- src/router/media.js
+++ src/router/media.js
@@ -1,10 +1,16 @@
 import List from '@/views/media/List.vue'
+import Permissions from '@/views/media/Permissions.vue'
 
 const routes = [
   {
     path: '/media',
     name: 'MediaList',
     component: List,
+  },
+  {
+    path: '/media/:mediaId/permissions',
+    name: 'MediaPermissions',
+    component: Permissions,
   },
 ]
 
