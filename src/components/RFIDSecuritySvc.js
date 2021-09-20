02516abdb2ef69a458b915fa7809257d3469b364
“Brian <bcurnow@users.noreply.github.com>
2021-09-20 01:14:50 +0100

    Added sound and player services

--- src/components/RFIDSecuritySvc.js
+++ src/components/RFIDSecuritySvc.js
@@ -3,6 +3,8 @@ import mediaSvc from './svc/Media.js'
 import permissionSvc from './svc/Permission.js'
 import readerSvc from './svc/Reader.js'
 import associationSvc from './svc/Association.js'
+import soundSvc from './svc/Sound.js'
+import playerSvc from './svc/Player.js'
 
 // TODO remove theses error, I don't think we'll use them
 export class ApiError extends Error {
@@ -91,7 +93,9 @@ const RFIDSecuritySvc = {
   errorToString: errorToString,
   media: mediaSvc,
   permission: permissionSvc,
+  player: playerSvc,
   reader: readerSvc,
+  sound: soundSvc,
 }
 
 export default RFIDSecuritySvc
