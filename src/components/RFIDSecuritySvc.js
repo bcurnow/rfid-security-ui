9c782d9c2bbe85cfe4fce03da4bd8fb627a1b467
“Brian <bcurnow@users.noreply.github.com>
2021-10-25 22:16:26 +0100

    Removed associationSvc and reordered other inports, added guests and mediaperms

--- src/components/RFIDSecuritySvc.js
+++ src/components/RFIDSecuritySvc.js
@@ -1,17 +1,19 @@
 import configSvc from './svc/Config.js'
-import mediaSvc from './svc/Media.js'
-import permissionSvc from './svc/Permission.js'
-import readerSvc from './svc/Reader.js'
-import associationSvc from './svc/Association.js'
-import soundSvc from './svc/Sound.js'
-import playerSvc from './svc/Player.js'
+import guestSvc from './svc/Guests.js'
 import errorToString from '@/components/Error.js'
+import mediaSvc from './svc/Media.js'
+import mediaPermSvc from './svc/MediaPerms.js'
+import permissionSvc from './svc/Permission.js'
+import playerSvc from './svc/Player.js'
+import readerSvc from './svc/Reader.js'
+import soundSvc from './svc/Sound.js'
 
 const RFIDSecuritySvc = {
-  association: associationSvc,
   config: configSvc,
   errorToString: errorToString,
+  guests: guestSvc,
   media: mediaSvc,
+  mediaPerms: mediaPermSvc,
   permission: permissionSvc,
   player: playerSvc,
   reader: readerSvc,
