c9b0a37a9723fdbc8bb9e64f2f023427c86addbf
“Brian <bcurnow@users.noreply.github.com>
2021-10-27 15:07:49 +0100

    Added Config model class and update get/list to convert to the model class

--- src/components/svc/Config.js
+++ src/components/svc/Config.js
@@ -1,5 +1,24 @@
 import api from './Base.js'
+import {BaseModel} from './Base.js'
 import combineURLs from 'axios/lib/helpers/combineURLs.js'
+
+export class Config extends BaseModel {
+  static type = 'Config'
+  static primaryKey = 'key'
+
+  constructor(api) {
+    super()
+    this.key = api.key
+    this.value = api.value
+  }
+
+  isControllable() {
+    if (this.key === 'ADMIN_API_KEY') {
+      return false
+    }
+    return true
+  }
+}
 
 const BASE_URL = '/configs'
 
@@ -11,11 +30,13 @@ const svc = {
   delete: function(key) {
     return api.delete(combineURLs(BASE_URL, key), {})
   },
-  get: function(key) {
-    return api.get(combineURLs(BASE_URL, key), {})
+  get: async function(key) {
+    const response = await api.get(combineURLs(BASE_URL, key), {})
+    return new Config(response.data)
   },
-  list: function() {
-    return api.get(BASE_URL, {})
+  list: async function() {
+    const response = await api.get(BASE_URL, {})
+    return response.data.map(api => new Config(api))
   },
   update: function(key, value) {
     return api.put(combineURLs(BASE_URL, key), {
