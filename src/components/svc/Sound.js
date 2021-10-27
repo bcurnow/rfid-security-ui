44c52dd985077599c8b46049f886a61cd16af895
“Brian <bcurnow@users.noreply.github.com>
2021-10-27 16:50:57 +0100

    Added conversion to the Sound model

--- src/components/svc/Sound.js
+++ src/components/svc/Sound.js
@@ -1,5 +1,19 @@
 import api from './Base.js'
+import {BaseModel} from './Base.js'
 import combineURLs from 'axios/lib/helpers/combineURLs.js'
+
+export class Sound extends BaseModel {
+  static type = 'Sound'
+  static primaryKey = 'id'
+
+  constructor(api) {
+    super()
+    this.id = api.id
+    this.name = api.name
+    this.lastUpdateTimestamp = api.last_update_timestamp
+    this.content = api.content
+  }
+}
 
 const BASE_URL = '/sounds'
 
@@ -11,11 +25,13 @@ const svc = {
   delete: function(id) {
     return api.delete(combineURLs(BASE_URL, String(id)), {})
   },
-  get: function(id) {
-    return api.get(combineURLs(BASE_URL, String(id)), {})
+  get: async function(id) {
+    const response = await api.get(combineURLs(BASE_URL, String(id)), {})
+    return new Sound(response.data)
   },
-  list: function() {
-    return api.get(BASE_URL, {})
+  list: async function() {
+    const response = await api.get(BASE_URL, {})
+    return response.data.map(api => new Sound(api))
   },
   update: function(id, data) {
     return api.put(combineURLs(BASE_URL, id.toString()), data)
