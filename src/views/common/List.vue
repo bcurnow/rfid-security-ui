5af9ede884c410172bd584ea2eab42206f11b4b1
“Brian <bcurnow@users.noreply.github.com>
2021-10-27 16:59:58 +0100

    Reduced debounce to improve perceived performance

--- src/views/common/List.vue
+++ src/views/common/List.vue
@@ -50,7 +50,7 @@
                     <slot name="thead-filter">
                       <b-form-group class="mb-0" label="Filter" label-for="filter-input" label-cols="auto" label-size="sm">
                         <b-input-group size="sm">
-                          <b-form-input id="filter-input" v-model="tableData.filter" type="search" placeholder="Type to Search" debounce="1000"></b-form-input>
+                          <b-form-input id="filter-input" v-model="tableData.filter" type="search" placeholder="Type to Search" debounce="250"></b-form-input>
                           <b-input-group-append>
                             <b-button :disabled="!tableData.filter" @click="tableData.filter = ''">Clear</b-button>
                           </b-input-group-append>
