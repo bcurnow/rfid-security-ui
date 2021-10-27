234d7663e2463e85a8c9ced4475c25713fc03266
“Brian <bcurnow@users.noreply.github.com>
2021-10-27 19:47:03 +0100

    Made the service interface a proper module

---
+++ src/components/rfidsecuritysvc/index.js
@@ -0,0 +1,23 @@
+import configSvc from "./Config"
+import guestSvc from "./Guests"
+import errorToString from "@/components/Error.js"
+import mediaSvc from "./Media"
+import mediaPermSvc from "./MediaPerms"
+import permissionSvc from "./Permission"
+import playerSvc from "./Player"
+import readerSvc from "./Reader"
+import soundSvc from "./Sound"
+
+const RFIDSecuritySvc = {
+  config: configSvc,
+  errorToString: errorToString,
+  guests: guestSvc,
+  media: mediaSvc,
+  mediaPerms: mediaPermSvc,
+  permission: permissionSvc,
+  player: playerSvc,
+  reader: readerSvc,
+  sound: soundSvc,
+}
+
+export default RFIDSecuritySvc
