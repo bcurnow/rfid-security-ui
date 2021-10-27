d1271f99bad1f695bcd64ba838985d683bd91aa8
“Brian <bcurnow@users.noreply.github.com>
2021-10-27 15:09:03 +0100

    Renamed forPGroupProps to props, updated to use the new interface for common list

--- src/views/config/List.vue
+++ src/views/config/List.vue
@@ -4,20 +4,17 @@
       :createItemPromise="createItemPromise"
       :deleteItemPromise="deleteItemPromise"
       :fields="fields"
-      id="Config"
+      :itemClass="itemClass"
       :itemsPromise="itemsPromise"
-      itemType="Configuration"
-      :noControlsForPKs="noControlsForPKs"
-      primaryKey="key"
       :updateItemPromise="updateItemPromise"
       :validationStates="validationStates">
-      <template #formGroups="formGroupsProps">
+      <template #formGroups="props">
         <b-form-group label="Key:" label-for="key-input" invalid-feedback="A configuration key is required!">
-          <b-form-input id="key-input" v-model="formGroupsProps.item.key" v-if="!formGroupsProps.isNew" readonly></b-form-input>
-          <b-form-input id="key-input" v-model="formGroupsProps.item.key" v-if="formGroupsProps.isNew" :state="validationStates.key" @invalid="validationStates.key = false" placeholder="Config Key" required></b-form-input>
+          <b-form-input id="key-input" v-model="props.item.key" v-if="!props.isNew" readonly></b-form-input>
+          <b-form-input id="key-input" v-model="props.item.key" v-if="props.isNew" :state="validationStates.key" @invalid="validationStates.key = false" placeholder="Config Key" required></b-form-input>
         </b-form-group>
         <b-form-group label="Value:" label-for="value-input" invalid-feedback="A configuration value is required!">
-          <b-form-input id="value-input" v-model="formGroupsProps.item.value" :state="validationStates.value" @invalid="validationStates.value = false" placeholder="Config Value" required></b-form-input>
+          <b-form-input id="value-input" v-model="props.item.value" :state="validationStates.value" @invalid="validationStates.value = false" placeholder="Config Value" required></b-form-input>
         </b-form-group>
       </template>
     </item-list>
@@ -25,16 +22,13 @@
 </template>
 <script>
   import List from '../common/List'
-
+  import {Config} from '@/components/svc/Config.js'
   export default {
     components: {
       'item-list': List,
     },
     data() {
       return {
-        noControlsForPKs: [
-          'ADMIN_API_KEY',
-        ],
         fields: [
           {
             key: 'key',
@@ -50,6 +44,7 @@
             label: '',
           }
         ],
+        itemClass: Config,
         validationStates: {
           key: null,
           value: null,
