87f59db6fe7613e694a1a793f7709d793a3e6a80
“Brian <bcurnow@users.noreply.github.com>
2021-10-27 15:08:00 +0100

    Removed testing code

--- src/components/svc/MediaPerms.js
+++ src/components/svc/MediaPerms.js
@@ -6,13 +6,6 @@ const BASE_URL = '/media-perms'
 const svc = {
   api: api,
   create: function(data) {
-    /*
-    if (data.permission_id == 1) {
-      return new Promise(() => {
-        throw new Error("Testing!")
-      })
-    }
-    */
     return api.post(BASE_URL, data)
   },
   delete: function(id) {
