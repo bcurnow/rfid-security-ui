f4e5f535267a114e55e9861b603b578ec8b3232f
“Brian <bcurnow@users.noreply.github.com>
2021-10-27 15:11:02 +0100

    Replaced == with === to follow best practices

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   devServer: {
-    disableHostCheck: process.env.WEBPACK_DISABLE_HOST_CHECK == 'true' || false,
+    disableHostCheck: process.env.WEBPACK_DISABLE_HOST_CHECK === 'true' || false,
     historyApiFallback: true,
     host: '0.0.0.0',
     hot: true,
-    liveReload: process.env.WEBPACK_LIVE_RELOAD == 'true' || false,
+    liveReload: process.env.WEBPACK_LIVE_RELOAD === 'true' || false,
     public: process.env.WEBPACK_PUBLIC || 'localhost:8080',
     proxy: {
       '/api': {
-        target: 'http://rfidsecuritysvc:5000',  
+        target: 'http://rfidsecuritysvc:5000',
       },
     },
   },
