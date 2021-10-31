b6fe8b3ab793f19f3e326c242a287c4ca7240c82
“Brian <bcurnow@users.noreply.github.com>
2021-10-31 02:23:23 +0000

    Added GuestMedia child component to manage guest to media associations

--- src/router/guests.js
+++ src/router/guests.js
@@ -1,10 +1,18 @@
 import List from '@/views/guests/List.vue'
+import Media from '@/views/guests/Media.vue'
 
 const routes = [
   {
     path: '/guests',
     name: 'GuestsList',
     component: List,
+    children: [
+      {
+        path: ':guestId/media',
+        name: 'GuestMedia',
+        component: Media,
+      },
+    ],
   },
 ]
 
