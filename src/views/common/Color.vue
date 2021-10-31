1ff57cb2b82777178ab62e0db4ebf9d2e707cda1
“Brian <bcurnow@users.noreply.github.com>
2021-10-31 03:19:02 +0000

    Created a custom component to enable showing a color swatch

---
+++ src/views/common/Color.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div v-if='color != null' :style='toColorStyle(color.html)' class='w-100' v-b-tooltip.v-primary='`${color.html}`'>&nbsp;</div>
+    <p v-if='color === null' class='text-muted'>{{ nullColorValue }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+    toColorStyle(color) {
+      if (color) {
+        return `background-color: ${color}`
+      }
+    },
+  },
+  props: {
+    color: {
+      type: Object,
+      required: true,
+    },
+    nullColorValue: {
+      type: String,
+      default: '<default>'
+    }
+  }
+}
+</script>
