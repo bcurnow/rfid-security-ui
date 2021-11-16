401aed4d30058a963d77ca73addb2aeb45261b77
“Brian <bcurnow@users.noreply.github.com>
2021-11-16 13:14:38 -0500

    Removed the apikey (this will now be handled by the proxy) and updated the default for the API URL to assume a proxied path

--- src/config/config.production.js
+++ src/config/config.production.js
@@ -1,7 +1,6 @@
 const config = {
-  apiKey: process.env.VUE_APP_RFID_API_KEY || 'Wu0sWRGCLF1NClIsYZVCdc-MV8b696oivfFF1oQqCGnK0vEaUZyb80El0ln6eCWKO9qrNlNTmvOfb7q0-GtnTw',
   production: true,
-  apiUrl: process.env.VUE_APP_RFID_API_URL || 'http://devpi.local:8080/api/v1.0/'
+  apiUrl: process.env.VUE_APP_RFID_API_URL || '/api/v1.0/'
 }
 
 export default config
