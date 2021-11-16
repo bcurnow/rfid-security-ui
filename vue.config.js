6c8da35497f078e96901bf749175bd07b0d34ab7
“Brian <bcurnow@users.noreply.github.com>
2021-11-16 10:32:11 -0500

    Removed devtool configuration, vue.js already handles this

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,6 @@ const webpack = require('webpack')
 
 module.exports = {
   configureWebpack: {
-    devtool: process.env.NODE_ENV == 'production' ? false : 'eval',
     plugins: [
         new webpack.DefinePlugin({
             'process.env.PACKAGE_VERSION': JSON.stringify(version),
